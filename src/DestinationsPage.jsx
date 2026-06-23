563dac6b153ad7d90577897957d2f11a9bd0806d
BreakinBenny <fox.wernicke@gmail.com>
2026-06-23 15:31:21 +0200

    Putting up my DestinationsPage

---
+++ src/DestinationsPage.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './DestinationsPage.css'
+
+function DestinationsPage() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center" style={{backgroundColor: "orange" }}>
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs" style={{backgroundColor: "lightblue", marginLeft: 60}}>
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+        </div>
+        <div id="social" style={{backgroundColor: "yellow", marginLeft: 320}}>
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default DestinationsPage;
