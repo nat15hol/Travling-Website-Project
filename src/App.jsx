6d68b035af23ac35f2ecca1fa7e1142857d3a402
Henrik Oldehed <henrik.oldehed@gmail.com>
2026-06-24 09:48:16 +0200

    fix leftover merge conflict markers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 import SearchLandingPage from "./pages/SearchLandingPage";
 import Login from "./Pages/Login";
-=======
-import SearchLandingPage from "./Pages/SearchLandingPage";
->>>>>>> e90e0f1384c00caf3662bb4b45d000b6116e6e60
+
 
 function App() {
   const showLogin = true;
