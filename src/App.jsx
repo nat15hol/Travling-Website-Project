213753c8046420a2624996015b75f2445710a500
wikman776 <fransman192@hotmail.com>
2026-06-29 11:34:48 +0200

    Added Confirmation and booking pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
 import SearchLandingPage from "./Pages/SearchLandingPage";
 import HomePage from "./Pages/homePage";
+import BookingPage from "./Pages/BookingPage";
+import ConfirmationPage from "./Pages/ConfirmationPage";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [currentPage, setCurrentPage] = useState("home");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentPage, setCurrentPage] = useState("HomePage");
   const [searchData, setSearchData] = useState({ city: "London", guests: 2,checkInDate: null, checkOutDate: null,});
+  const [selectedDestination, setSelectedDestination] = useState(null);
 
   if (!isLoggedIn) {
     if (currentPage === "signup") { return <SignupPage setCurrentPage={setCurrentPage} />; }
@@ -16,8 +19,12 @@ function App() {
   }
 
   if (currentPage === "search") { return <SearchLandingPage setCurrentPage={setCurrentPage} searchData={searchData} />; }
+  if (currentPage === "booking") { return (<BookingPage setCurrentPage={setCurrentPage} selectedDestination={selectedDestination}/>); }
+  if (currentPage === "confirmation") { return (<ConfirmationPage setCurrentPage={setCurrentPage} selectedDestination={selectedDestination}/>  );
+}
 
   return <HomePage setCurrentPage={setCurrentPage} setSearchData={setSearchData}/>;
+
 }
 
 export default App;
